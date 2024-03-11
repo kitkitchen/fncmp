096586eef86076f83a517aec64dbb5484b7f411a
Sean Burman <sean.burman@icloud.com>
2024-03-11 17:23:31 +0000

    Updated cache with get and set method with optional timeout

--- cache.go
+++ cache.go
@@ -12,17 +12,30 @@ type Cache[T any] struct {
 	cacheKey  interface{}
 	createdAt time.Time
 	updatedAt time.Time
+	timeOut   time.Duration
 	data      T
 }
 
-// Set sets the value of the cache
-func (c *Cache[T]) Set(data T) error {
+// Set sets the value of the cache with a timeout
+//
+// Set timeout to 0 for no expiry.
+func (c *Cache[T]) Set(data T, timeout time.Duration) error {
 	c.data = data
-	_, err := getCache[T](c.storeKey, c.cacheKey)
+	cache, err := getCache[T](c.storeKey, c.cacheKey)
 	if err != nil && !errors.Is(err, ErrCacheNotFound) {
 		return err
 	}
+
+	c.timeOut = timeout
+
+	// If updatedAt is zero, the cache is new
+	// start expiry watcher
+	if cache.updatedAt.IsZero() {
+		c.updatedAt = time.Now()
+		go c.watchExpiry()
+	}
 	c.updatedAt = time.Now()
+
 	setCache(c.storeKey, c.cacheKey, &data)
 	return nil
 }
@@ -41,32 +54,74 @@ func (c *Cache[T]) Delete() {
 	deleteCache(c.storeKey, c.cacheKey)
 }
 
+// CreatedAt returns the time the cache was created
 func (c *Cache[T]) CreatedAt() time.Time {
 	return c.createdAt
 }
 
+// UpdatedAt returns the time the cache was last updated
 func (c *Cache[T]) UpdatedAt() time.Time {
 	return c.updatedAt
 }
 
-// UseCache returns a cache from the store.
+// Expiry returns expiry time of the cache
+func (c *Cache[T]) Expiry() time.Time {
+	if c.updatedAt.IsZero() {
+		return c.createdAt.Add(c.timeOut)
+	}
+	return c.updatedAt.Add(c.timeOut)
+}
+
+// watchExpiry is a goroutine that watches the cache for expiry
+// and deletes the cache when it expires
+func (c *Cache[T]) watchExpiry() {
+	if c.timeOut == 0 {
+		return
+	}
+	for {
+		time.Sleep(c.timeOut)
+		if time.Now().After(c.Expiry()) {
+			c.Delete()
+			break
+		}
+	}
+}
+
+// UseCache takes a generic type and a key and returns a getter, setter, and error for cache of that type.
+// UseCache can be accessed globally and is concurrency safe.
 //
-// * It is important that the context passed to UseCache is that from a HandleFn
+// If the cache does not exist, it will be created. Calling the getter on a newly created cache will return the zero value of the generic type.
+//
+// If the cache exists, it will be returned and the getter can be called directly to get the current value.
+//
+// If the cache exists but the generic type is different, an error will be returned.
+//
+// The returned setter can be called to update the cache with a new value and timeout.
+// A timeout of 0 will set the cache to never expire, that is until the the user disconnects* or the server is restarted.
+//
+// * The default timeout for a user's cache is 30 minutes after disconnection. To extend the timeout, the user must reconnect before the timeout expires,
+// or SetConfig can be called to change the default timeout.
+//
+// ! It is important that the context passed to UseCache is that from a HandleFn
+// as it is used to retrieve Dispatch. An error will be returned if the context is missing Dispatch. !
 //
 // https://pkg.go.dev/github.com/kitkitchen/fncmp#HandleFn
-func UseCache[T any](ctx context.Context, key interface{}) (Cache[T], error) {
+func UseCache[T any](ctx context.Context, key interface{}) (get func() T, set func(T, time.Duration) error, err error) {
 	dispatch, ok := dispatchFromContext(ctx)
 	if !ok {
-		return Cache[T]{}, ErrCtxMissingDispatch
+		return nil, nil, ErrCtxMissingDispatch
 	}
 	cache, err := getCache[T](dispatch.ConnID, key)
 	if errors.Is(err, ErrCacheNotFound) {
 		newCache[T](dispatch.ConnID, key)
 		cache, err := getCache[T](dispatch.ConnID, key)
-		return cache, err
+		return cache.Value, cache.Set, err
 	}
-	return cache, err
+	// return cache getter, setter, and error for ErrCtxMissingDispatch or ErrCacheWrongType
+	return cache.Value, cache.Set, err
 }
+
+// NOTE: The following is some rewritten logic from package mnemo and will be extracted.
 
 var sm = storeManager{
 	stores: make(map[interface{}]*store),
